71246075cfeacf41124581e8bc22a1306757c027
Hicham1970 <devhicham2@gmail.com>
2024-10-13 10:43:02 +0100

    Add ancient components and the Router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import Todos from "./Todos/Todos";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [count, setCount] = useState(0);
+  // eslint-disable-next-line no-unused-vars
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <div className="container">
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <br></br>
+        <button className="btn" onClick={increment}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
