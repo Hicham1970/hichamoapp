0bcb6e10fccbaaa667c4e18c10e84559b850edf6
Hicham1970 <devhicham2@gmail.com>
2024-10-22 17:56:17 +0100

    UseContext and UseRef hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Application from "./Hooks/UseRef"
+import Application2 from "./Hooks/UseRef2"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Application />
+    <Application2 />
+    </>
+    
   );
 }
 
